c585299d81f0c3dd7917c27b6d8d6961fb79266d
wmartin74 <martin74@iastate.edu>
2025-08-04 13:40:22 -0500

    script v1.1

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function pullfdata() {
     try {
         const submittedName = parseFloat(document.getElementById("name").value);
-        }
-    
         return submittedName;
-    
-    catch (error) {
+    } catch (error) {
         console.error("Error parsing input:", error);
         window.alert("Please enter a valid name.");
         return null;
     }
 }
+
+function runscript() {
+    const submittedname = pullfdata();
+    
+    window.location.href = "result.html";
+    }
+
