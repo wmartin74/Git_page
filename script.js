cff8f9dd9305fee05f06e8a32350f9a4ed8aeaff
wmartin74 <martin74@iastate.edu>
2025-08-05 15:12:48 -0500

    script v1.6.7

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function pullfdata() {
 
 function runscript(event) {
     event.preventDefault(); // Prevent the default form submission behavior
-    let submittedName = pullfdata();
+    let submittedname = pullfdata();
     console.log(submittedName);
-    localStorage.setItem("submission", JSON.stringify(submittedName));
+    localStorage.setItem("submission", JSON.stringify(submittedname));
     console.log("saved submission");
     window.location.href = "result.html";
     }
