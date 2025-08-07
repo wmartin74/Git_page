5cd15e6dad9ccc963d3c7a1352e2a5326bc8b206
wmartin74 <martin74@iastate.edu>
2025-08-07 09:50:31 -0500

    Create result.js

---
+++ Scripts/result.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submittedname = document.getElementById("submittedname")
+  console.log("Loaded submission")
+}
+                          
