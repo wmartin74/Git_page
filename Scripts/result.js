c9135ea46e696e793c9c88f0ed6b0bdeedcd3994
wmartin74 <martin74@iastate.edu>
2025-08-07 10:24:07 -0500

    result v1.1.5

--- Scripts/result.js
+++ Scripts/result.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  submittedname = JSON.parse(localStorage.getItem("submission"));
+  submittedname.innerHTML = JSON.parse(localStorage.getItem("submission"));
   console.log("Loaded submission");
   console.log(submittedname);
 });
